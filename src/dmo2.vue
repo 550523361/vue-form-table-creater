7fca40dd056157bb6cc491ffacac6e178b9f2456
fengxiankun <550523361@qq.com>
2019-04-01 14:39:36 +0800

    test

---
+++ src/dmo2.vue
@@ -0,0 +1,19 @@
+<template>
+		<div>
+			{{name}}
+		</div>
+</template>
+
+<script>
+	export default {
+		name:'hbuilderx',
+		data:function(){
+			return {
+				name:"demo name1"
+			}
+		}
+	}
+</script>
+
+<style>
+</style>
